42a459599407f8f60d9d6b82392b02977857a623
Xander Pope <xander.pope@ukaea.uk>
2025-06-02 15:56:44 +0100

    Code style fixes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -13,15 +13,17 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    in_files = args.infiles
+    if not isinstance(in_files, list):
+        in_files = [args.infiles]
 
 
-    for filename in InFiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data), 
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
 
@@ -34,6 +36,6 @@ if __name__ == "__main__":
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
 
-    args = parser.parse_args()
+    arguments = parser.parse_args()
 
-    main(args)
+    main(arguments)
